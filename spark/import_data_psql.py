5e0fcb70a1a796d94ceb9a7807e8b8e8a79e6ad4
Eve Chen <yuanyichen1@gmail.com>
2019-01-31 15:09:20 -0800

    Update and rename 1_data_from_s3.py to import_data_psql.py

---
+++ spark/import_data_psql.py
@@ -0,0 +1,33 @@
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SQLContext, SparkSession
+import os
+
+def get_s3_data(bucket_name, file_name = None):
+    path = 's3a://{}/'.format(bucket_name)
+    if file_name is not None:
+        path += '{}/'.format(file_name)
+
+    return path
+
+bucket = "amazon-reviews-insight"
+path = get_s3_data(bucket, "reviews_Amazon_Instant_Video.json")
+
+conf = SparkConf().setAppName("AmazonReviewDashboard") 
+sc = SparkContext(conf = conf)
+sqlContext = SQLContext(sc)
+df = sqlContext.read.format('json').load(path)
+
+
+
+#PostgreSQL
+
+
+properties = {'user': 'postgres', 'password': '3890', 'driver': 'org.postgresql.Driver'}
+mode = "overwrite"
+url = "jdbc:postgresql://34.209.104.124:5432/my_db"
+
+try:
+    df.write.jdbc(url = url, table="fulldata", mode = mode, properties = properties)
+    print("If you see this line, it means the jdbc write function did not crash")
+except Exception as e:
+    print(e)
