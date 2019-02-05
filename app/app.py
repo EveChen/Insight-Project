e0b68efe5eac2ade52098ff8bb886b6771937932
Eve Chen <yuanyichen1@gmail.com>
2019-02-05 10:21:23 -0800

    Create app.py

---
+++ app/app.py
@@ -0,0 +1,31 @@
+# Initiate my Flask app
+
+# Packages
+from flask import Flask
+from flaskext.mysql import MySQL
+from flask import render_template
+
+app = Flask(__name__)
+
+# set up connection
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'newuser'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Xiavi293@'
+app.config['MYSQL_DATABASE_DB'] = 'my_db'
+app.config['MYSQL_DATABASE_HOST'] = 'ec2-34-213-6-190.us-west-2.compute.amazonaws.com'
+mysql.init_app(app)
+
+
+@app.route("/")
+def show_top_five():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM reviews LIMIT 5")
+    results = cursor.fetchall()
+    print(results)
+    print(type)
+    return render_template("index.html", results = results)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
