73b3bfda022fbd1a4cb1ab3ce5eeb889533d9c2b
Eve Chen <yuanyichen1@gmail.com>
2019-02-14 15:44:22 -0800

    Query much faster

--- app/app.py
+++ app/app.py
@@ -10,8 +10,8 @@ from dash.dependencies import Output, Input, State
 
 # Connect MySQL with Dash
 def fetchData(command):
-    conn = MySQLdb.connect(host="publicDNS",
-            user="username", passwd="password", db="dbname")
+    conn = MySQLdb.connect(host="ec2-34-213-6-190.us-west-2.compute.amazonaws.com",
+            user="newuser", passwd="Xiavi293@", db="my_db")
     try:
         # connect to the MySQL server
         cur = conn.cursor()
@@ -26,10 +26,12 @@ def fetchData(command):
             conn.close()
     return rows
 
-
-# Note: dash's category has the column name "imUrl" instead of "category" (would be fixed later)
+# Note: dash's category has the tag "imUrl" instead of "category"
 def get_data(input_text):
-    command = "SELECT title, price, imUrl, original_ratings, adjusted_ratings, label FROM dash WHERE title LIKE '%{}%'".format(input_text)
+    if len(input_text) <= 0:
+        command = "SELECT title, price, category, original_ratings, adjusted_ratings, label FROM meta_index JOIN reviews_index ON meta_index.asin = reviews_index.asin WHERE title IS NOT NULL AND price IS NOT NULL LIMIT 10"
+    else:
+        command = "SELECT title, price, category, original_ratings, adjusted_ratings, label FROM meta_index JOIN reviews_index ON meta_index.asin = reviews_index.asin WHERE title LIKE '%{}%' AND price IS NOT NULL LIMIT 50".format(input_text)
     return fetchData(command)
 
 
@@ -37,7 +39,7 @@ def get_data(input_text):
 def generate_table(input_text):
     data = get_data(input_text)
     df = pd.DataFrame([ij for ij in i] for i in data)
-    df.rename(columns = {0 : "Product Name", 1: "Price", 2: "Category", 3: "Original_ratings", 4 : "Adjusted_ratings", 5: "Label"}, inplace = True)
+    df.rename(columns = {0 : "Product Name", 1: "Price", 2: "Category", 3: "Original Ratings", 4 : "Adjusted Ratings", 5: "Label"}, inplace = True)
 
     return html.Table(
         # Header
@@ -49,27 +51,24 @@ def generate_table(input_text):
         ]) for i in range(len(df))]
     )
 
-# Create the Dash app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# Design the layout
+
 app.layout = html.Div([
     html.H1(children='''
         Amazon Adjusted Ratings
     '''),
     dcc.Input(id='input-productname', type='text', value=''),
-    # html.Button(id='submit-button', n_clicks=0, children='Submit'), #Haven't done the submit button part
+    # html.Button(id='submit-button', n_clicks=0, children='Submit'),
     html.Div(id='output-query')
 ])
 
 
-# Callback: use the input value as the key to query product information
 @app.callback(Output('output-query', 'children'),
               [Input('input-productname', 'value')])
 def update_output(input):
     return generate_table(input)
-
 
 
 if __name__ == '__main__':
